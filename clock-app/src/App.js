267835796ee30f336a03f1d8e222e35177b88a6e
bsihame <sihamebazi@pursuit.org>
2020-04-17 22:03:51 -0400

    app without toggle button

--- clock-app/src/App.js
+++ clock-app/src/App.js
@@ -4,17 +4,17 @@ import Clock from "./components/Clock";
 
 class App extends React.Component {
 	state = { showClock: true };
-	handleToggle = () => {
-		this.setState((prevState) => ({
-			showClock: !prevState.showClock,
-		}));
-	};
+	// handleToggle = () => {
+	// 	this.setState((prevState) => ({
+	// 		showClock: !prevState.showClock,
+	// 	}));
+	// };
 	render() {
 		return (
 			<div className="App">
 				<h2>Try Our Clock</h2>
 				<div className="clock">{this.state.showClock ? <Clock /> : null}</div>
-				<button onClick={this.handleToggle}>Toggle Clock</button>
+				{/* <button onClick={this.handleToggle}>Toggle Clock</button> */}
 			</div>
 		);
 	}
