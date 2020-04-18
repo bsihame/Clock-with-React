13bfe612126a9ff8d036dbf83763dc8c03a6658e
bsihame <sihamebazi@pursuit.org>
2020-04-17 21:46:53 -0400

    remove logo from app js file

--- clock-app/src/App.js
+++ clock-app/src/App.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Clock from "./components/Clock"
+import React from "react";
+import "./App.css";
+import Clock from "./components/Clock";
 
 class App extends React.Component {
-  state = { showClock: true };
-  handleToggle = () => {
-    this.setState((prevState) => ({
-      showClock: !prevState.showClock
-    }))
-  }
-  render() {
-    return (
-      <div className="App">
-
-        <h2>Try Our Clock</h2>
-        <div className="clock">
-          {this.state.showClock ? <Clock /> : null}
-        </div>
-        <button onClick={this.handleToggle} >Toggle Clock</button>
-      
-      </div>
-    );
-  }
+	state = { showClock: true };
+	handleToggle = () => {
+		this.setState((prevState) => ({
+			showClock: !prevState.showClock,
+		}));
+	};
+	render() {
+		return (
+			<div className="App">
+				<h2>Try Our Clock</h2>
+				<div className="clock">{this.state.showClock ? <Clock /> : null}</div>
+				<button onClick={this.handleToggle}>Toggle Clock</button>
+			</div>
+		);
+	}
 }
 
 export default App;
