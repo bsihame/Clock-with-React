bb7324d26821997b137dfea43116069958d6a592
bsihame <sihamebazi@pursuit.org>
2020-04-17 20:02:23 -0400

    change code of clock.js

---
+++ clock-app/src/components/Clock.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: new Date().toLocaleDateString()
+    }
+  }
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      console.log(new Date().toLocaleString());
+      this.setState({time: new Date ().toLocaleTimeString()})
+      
+    }, 1000)
+  }
+  componentWillMount() {
+    clearInterval(this.timer)
+  }
+  render() {
+    return (
+      <div>
+        {this.state.time}
+      </div>
+    )
+  }
+}
+export default Clock;
